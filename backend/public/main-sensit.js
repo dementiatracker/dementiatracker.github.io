9a15c59e3948eaf4054fa1992affee3beb20b1c6
dementiatracker <1672237C@student.tp.edu.sg>
2017-07-30 11:41:51 +0000

    Play with the data using a slider

--- backend/public/main-sensit.js
+++ backend/public/main-sensit.js
@@ -7,6 +7,12 @@ var g_DEVICE_IDS = {
 var g_PATIENT_ID = null;
 var g_DEVICE_ID  = null;
 
+var g_DATA       = null;
+var g_DATA_KEYS  = null;
+
+var g_MAP        = null;
+var g_MARKER     = null;
+
 // Helper Functions
 function getUrlParameter(name) {
     name        = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -19,66 +25,45 @@ function sendAlarm(msg) {
   $('#myinfo').text(msg).addClass('alert-danger').removeClass('alert-success').removeClass('alert-warning');
 }
 function showMsg(msg) {
-  $('#myinfo').text(msg).addClass('alert-success').removeClass('alert-danger').removeClass('alert-warning');
+  $('#myinfo').html(msg).addClass('alert-success').removeClass('alert-danger').removeClass('alert-warning');
 }
 
-function getMapUpdater(myMap) {
-  var myMarker  = null;
-  var myPolygon = null;
+function showPatientOnMap(timestamp) {
+  if(g_MAP === null || ! (timestamp in g_DATA)) return;
 
-  return function(timestamp, lat, lng) {
-    if(!timestamp || ! lat || ! lng) {
-      return;
-    }
+  var item = g_DATA[timestamp];
+  if(! ('lat' in item) || ! ('lng' in item)) return;
 
-    if(myMarker === null) {
-      myMarker = myMap.addMarker({
-        label: 'Patient ' + g_PATIENT_ID,
-        lat:   lat,
-        lng:   lng
-      });
-    }
+  if(g_MARKER === null) {
+    g_MARKER = g_MAP.addMarker({
+      lat:  item['lat'],
+      lng:  item['lng'],
+      icon: {
+        url:         "http://labs.google.com/ridefinder/images/mm_20_red.png",
+        size:        new google.maps.Size(12, 20),
+        scaledSize:  new google.maps.Size(12, 20),
+        labelOrigin: new google.maps.Point(0, 30)
+      }
+    });
+  }
 
-    myMarker.setPosition(new google.maps.LatLng(lat, lng));
-  };
+  g_MARKER.setPosition(new google.maps.LatLng(item['lat'], item['lng']));
+  g_MARKER.setLabel({
+    text:  moment.utc(parseInt(timestamp)).format("YYYY-MM-DD HH:mm:ss"),
+    color: "blue"
+  });
 }
 
-function showPatientOnMap(updateMap, results, timestamps) {
-  var i   = 0;
-  var len = timestamps.length;
-
-  var prefix = 'Patient ' + g_PATIENT_ID + ' (with ' + timestamps.length + ' data points): ';
-
-  var myTimer = setInterval(function() {
-    if(i >= len) {
-      clearInterval(myTimer);
-      showMsg(prefix + 'All data are shown');
-      return;
-    }
-
-    var timestamp = timestamps[i++];
-
-    // update info bar
-    showMsg(prefix + moment(parseInt(timestamp)).format());
-
-    var item = results[timestamp];
-    if(! ('lat' in item) || ! ('lng' in item)) return;
-
-    updateMap(timestamp, item['lat'], item['lng']);
-  }, 1000);
-}
-
-function fitMapBounds(results, timestamps) {
-  var myMap = null;
-
+function fitMapBounds() {
   var bounds = new google.maps.LatLngBounds();
-  timestamps.forEach(function(timestamp) {
-    var item = results[timestamp];
+
+  g_DATA_KEYS.forEach(function(timestamp) {
+    var item = g_DATA[timestamp];
 
     if(! ('lat' in item) || ! ('lng' in item)) return;
 
-    if(myMap === null) {
-      myMap = new GMaps({
+    if(g_MAP === null) {
+      g_MAP = new GMaps({
         div:     '#mymap',
         mapType: 'map',
         lat:     item['lat'],
@@ -89,36 +74,38 @@ function fitMapBounds(results, timestamps) {
     bounds.extend(new google.maps.LatLng(item['lat'], item['lng']));
   });
 
-  if(myMap !== null) {
-    myMap.fitBounds(bounds);
+  if(g_MAP !== null) {
+    g_MAP.fitBounds(bounds);
   }
-
-  return myMap;
 }
 
 function getData() {
-  var result = {};
+  showMsg('Loading data for patient' + g_PATIENT_ID);
 
   $.getJSON('http://ec2-34-210-200-155.us-west-2.compute.amazonaws.com/geodata/' + g_DEVICE_ID)
    .done(function(data) {
      if('code' in data && data['code'] == 200) {
-       delete data['code'];
+       g_DATA = data;
+       delete g_DATA['code'];
 
-       var timestamps = Object.keys(data);
-       if(timestamps.length <= 0) {
+       g_DATA_KEYS = Object.keys(g_DATA);
+       if(g_DATA_KEYS.length <= 0) {
          sendAlarm('Empty data for patient ' + g_PATIENT_ID);
          return;
        }
-       timestamps.sort();
+       g_DATA_KEYS.sort();
 
-       var myMap = fitMapBounds(data, timestamps);
-       if(myMap === null) {
+       showMsg('Patient ' + g_PATIENT_ID + ' (with ' + g_DATA_KEYS.length + ' data points loaded)<br/>' +
+               'From ' + moment.utc(parseInt(g_DATA_KEYS[0                     ])).format("YYYY-MM-DD HH:mm:ss") +
+               ' to '  + moment.utc(parseInt(g_DATA_KEYS[g_DATA_KEYS.length - 1])).format("YYYY-MM-DD HH:mm:ss"));
+
+       fitMapBounds();
+       if(g_MAP === null) {
          sendAlarm('Not enough data for patient ' + g_PATIENT_ID);
          return;
        }
 
-       var updateMap = getMapUpdater(myMap);
-       showPatientOnMap(updateMap, data, timestamps);
+       showPatientOnMap(getCurrentTimeStamp());
      }
      else {
        sendAlarm('Backend error when getting info for patient ' + g_PATIENT_ID);
@@ -129,6 +116,22 @@ function getData() {
     sendAlarm('Failed to get info for patient ' + g_PATIENT_ID);
     return;
   });
+}
+
+function getCurrentTimeStamp() {
+  var sliderVal = $('#myslider').slider("value");
+
+  if(g_DATA_KEYS === null) return null;
+
+  var pos = Math.floor(g_DATA_KEYS.length * sliderVal / 100);
+  if(pos < 0) pos = 0;
+  if(pos >= g_DATA_KEYS.length) pos = g_DATA_KEYS.length - 1;
+
+  return g_DATA_KEYS[pos];
+}
+
+function refreshMap() {
+  showPatientOnMap(getCurrentTimeStamp());
 }
 
 // Init Function
@@ -142,6 +145,19 @@ function init() {
 
   g_PATIENT_ID = patientID;
   g_DEVICE_ID  = g_DEVICE_IDS[g_PATIENT_ID];
+
+  document.title = 'Dementia Tracker - Patient ' + g_PATIENT_ID;
+  $('.navbar-header a').text(document.title);
+
+  $("#myslider").slider({
+    orientation: "horizontal",
+    range:       "min",
+    max:         100,
+    value:       0,
+    slide:       refreshMap,
+    change:      refreshMap
+  });
+  $("#myslider").slider("value", 0);
 
   getData();
 }
